a21511802f2f3237d462715f7c97eba3f548d3de
Rakan <RakanQ5@outlook.com>
2022-02-06 20:51:54 +0300

    cart style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,31 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    screen: {
-      'mobile': '375px',
-      'desktop': '1440px'
-    },
     extend: { 
+      screen: {
+        'lg': '1050px'
+      },
       colors: {
-        'orange': '#FF7D1A'
+        'orange': 'hsl(26, 100%, 55%)',
+        'pale-orange': 'hsl(25, 100%, 94%)',
+        'ver-dark-blue': 'hsl(220, 13%, 13%)',
+        'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+        'grayish-blue': 'hsl(220, 14%, 75%)',
+        'light-grayish-blue': 'hsl(223, 64%, 98%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'black' : 'hsl(0, 0%, 0%)'
       },
       fontFamily: {
         sans: ['"Kumbh Sans"', 'sans-serif']
+      },
+      width: {
+        '88': '22rem',
+        '112': '28rem',
+        '128': '32rem',
+      },
+      height: {
+        '112': '28rem',
+        '128': '32rem',
       }
     }
   },
